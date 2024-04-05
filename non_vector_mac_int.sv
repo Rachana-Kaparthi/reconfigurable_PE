ec13f7cda66efa72db2dddbae7d404e216ae3c51
Rachana.Kaparthi <rachana.kaparthi@iiitb.ac.in>
2024-04-05 14:17:57 +0530

    updates

--- non_vector_mac_int.sv
+++ non_vector_mac_int.sv
@@ -1,15 +1,16 @@
 `timescale 1ns/1ps
-module  non_vector_MAC_int #(parameter REG_WIDTH = 16) (clk,a_n_1,b_n_1,c_n_1,a_n,b_n,c_ab);
+module  non_vector_MAC_int #(parameter REG_WIDTH = 16) (clk,a_n_1,b_n_1,c_n_1,c_ab);
     
     input clk;
     input [REG_WIDTH-1:0] a_n_1;
     input [REG_WIDTH-1:0] b_n_1 ;
     input [REG_WIDTH-1:0] c_n_1;
-    output logic [REG_WIDTH-1:0] a_n; 
-    output logic [REG_WIDTH-1:0] b_n; 
+    // output logic [REG_WIDTH-1:0] a_n; 
+    // output logic [REG_WIDTH-1:0] b_n; 
     output logic [REG_WIDTH-1:0] c_ab;
 
-
-    assign  c_ab[j] <=  ( a_n_1 * b_n_1) + c_n_1;
-
+    always@(posedge clk) 
+        begin
+        c_ab <=  ( a_n_1 * b_n_1) + c_n_1;
+        end
 endmodule
